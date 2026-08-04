c014f838be1c17126da7c4fdd6a01fe0ceb0f065
Ihtisham Naseer <ihtishamnaseer07@gmail.com>
2026-08-04 16:29:25 +0300

    Fix Render deployment: switch bcrypt->bcryptjs, add socket.io-client to client, untrack dist/

--- server/src/controllers/adminPanelController.js
+++ server/src/controllers/adminPanelController.js
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import User from '../models/User.js';
 import Team from '../models/Team.js';
 import Match from '../models/Match.js';
--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import User from '../models/User.js';
 import { sendEmail } from '../utils/emailService.js';
 import { generateAuthToken, generateVerificationToken, generateResetToken } from '../utils/tokenService.js';
